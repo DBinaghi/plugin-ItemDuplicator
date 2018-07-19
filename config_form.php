58c9d78165c71df6a66d52c6581ca66aab0dca00
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2018-07-19 18:15:34 +0200

    Update config_form.php
    Added checkbox for DublinCore:Date field, and changed couple of descriptive strings.

--- config_form.php
+++ config_form.php
@@ -1,7 +1,8 @@
 <?php
-$item_duplicator_restricted		= get_option('item_duplicator_restricted');
+$item_duplicator_restricted	= get_option('item_duplicator_restricted');
 $item_duplicator_empty_title	= get_option('item_duplicator_empty_title');
-$item_duplicator_empty_tags		= get_option('item_duplicator_empty_tags');
+$item_duplicator_empty_date	= get_option('item_duplicator_empty_date');
+$item_duplicator_empty_tags	= get_option('item_duplicator_empty_tags');
 $view = get_view();
 ?>
 
@@ -19,7 +20,7 @@ $view = get_view();
 	</div>
 </div>
 
-<h2><?php echo __('Information duplication'); ?></h2>
+<h2><?php echo __('Information removal'); ?></h2>
 
 <div class="field">
 	<div class="two columns alpha">
@@ -27,9 +28,21 @@ $view = get_view();
 	</div>
 	<div class="inputs five columns omega">
 		<p class="explanation">
-			<?php echo __('If checked, field Title will be emptied when showing duplicate Item.'); ?>
+			<?php echo __('If checked, field DublinCore:Title will be emptied when showing duplicate Item.'); ?>
 		</p>
 		<?php echo $view->formCheckbox('item_duplicator_empty_title', $item_duplicator_empty_title, null, array('1', '0')); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('item_duplicator_empty_date', __('Empty Date field')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, field DublinCore:Date will be emptied when showing duplicate Item.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('item_duplicator_empty_date', $item_duplicator_empty_date, null, array('1', '0')); ?>
 	</div>
 </div>
 
