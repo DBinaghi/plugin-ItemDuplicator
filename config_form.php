c2d9836b5fbb0793c53abf616066dc286780af97
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2018-07-20 13:01:28 +0200

    Update config_form.php
    Added extra options

--- config_form.php
+++ config_form.php
@@ -1,8 +1,9 @@
 <?php
-$item_duplicator_restricted	= get_option('item_duplicator_restricted');
+$item_duplicator_restricted		= get_option('item_duplicator_restricted');
 $item_duplicator_empty_title	= get_option('item_duplicator_empty_title');
-$item_duplicator_empty_date	= get_option('item_duplicator_empty_date');
-$item_duplicator_empty_tags	= get_option('item_duplicator_empty_tags');
+$item_duplicator_empty_subject	= get_option('item_duplicator_empty_subject');
+$item_duplicator_empty_date		= get_option('item_duplicator_empty_date');
+$item_duplicator_empty_tags		= get_option('item_duplicator_empty_tags');
 $view = get_view();
 ?>
 
@@ -22,6 +23,7 @@ $view = get_view();
 
 <h2><?php echo __('Information removal'); ?></h2>
 
+
 <div class="field">
 	<div class="two columns alpha">
 		<?php echo $view->formLabel('item_duplicator_empty_title', __('Empty Title field')); ?>
@@ -31,6 +33,18 @@ $view = get_view();
 			<?php echo __('If checked, field DublinCore:Title will be emptied when showing duplicate Item.'); ?>
 		</p>
 		<?php echo $view->formCheckbox('item_duplicator_empty_title', $item_duplicator_empty_title, null, array('1', '0')); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('item_duplicator_empty_subject', __('Empty Subject field')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, field DublinCore:Subject will be emptied when showing duplicate Item.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('item_duplicator_empty_subject', $item_duplicator_empty_subject, null, array('1', '0')); ?>
 	</div>
 </div>
 
@@ -57,17 +71,3 @@ $view = get_view();
 		<?php echo $view->formCheckbox('item_duplicator_empty_tags', $item_duplicator_empty_tags, null, array('1', '0')); ?>
 	</div>
 </div>
-
-<h2><?php echo __('Test area'); ?></h2>
-
-<?php
-	$acl = get_acl();
-
-	$users = get_db()->getTable('User')->findAll();
-	foreach ($users as $key => $user):
-		if ($acl->isAllowed($user, 'Items', 'duplicate')):
-			// your action, like f.i.
-			echo $user->name . " (" . $user->role . ")<br>";
-		endif;
-	endforeach;	
-?>
