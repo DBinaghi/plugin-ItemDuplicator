8683071a70cf2535c0cb4f8cbb63e8faa4bf6811
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-07-29 17:22:39 +0200

    Refactor JavaScript initialization in form.php

--- views/admin/items/form.php
+++ views/admin/items/form.php
@@ -2,36 +2,36 @@
 <?php echo js_tag('elements'); ?>
 <?php echo js_tag('tabs'); ?>
 <?php echo js_tag('items'); ?>
+
 <script type="text/javascript" charset="utf-8">
-jQuery(document).ready(function () {
-    Omeka.Tabs.initialize();
+	jQuery(document).ready(function () {
+		Omeka.Tabs.initialize();
 
-    Omeka.Items.tagDelimiter = <?php echo js_escape(get_option('tag_delimiter')); ?>;
-    Omeka.Items.enableTagRemoval();
-    Omeka.Items.makeFileWindow();
-    Omeka.Items.enableSorting();
-    Omeka.Items.tagChoices('#tags', <?php echo js_escape(url(array('controller'=>'tags', 'action'=>'autocomplete'), 'default', array(), true)); ?>);
+		Omeka.Items.tagDelimiter = <?php echo js_escape(get_option('tag_delimiter')); ?>;
+		Omeka.Items.enableTagRemoval();
+		Omeka.Items.makeFileWindow();
+		Omeka.Items.enableSorting();
+		Omeka.Items.tagChoices('#tags', <?php echo js_escape(url(array('controller'=>'tags', 'action'=>'autocomplete'), 'default', array(), true)); ?>);
 
-    Omeka.wysiwyg({
-        selector: false,
-        forced_root_block: false
-    });
+		Omeka.wysiwyg({
+			selector: false,
+			forced_root_block: false
+		});
 
-    // Must run the element form scripts AFTER reseting textarea ids.
-    jQuery(document).trigger('omeka:elementformload');
+		// Must run the element form scripts AFTER reseting textarea ids.
+		jQuery(document).trigger('omeka:elementformload');
 
-    Omeka.Items.enableAddFiles(<?php echo js_escape(__('Add Another File')); ?>);
-    Omeka.Items.changeItemType(<?php echo js_escape(url("items/change-type")) ?><?php if ($id = metadata('item', 'id')) echo ', '.$id; ?>);
-});
+		Omeka.Items.enableAddFiles(<?php echo js_escape(__('Add Another File')); ?>);
+		Omeka.Items.changeItemType(<?php echo js_escape(url("items/change-type")) ?><?php if ($id = metadata('item', 'id')) echo ', '.$id; ?>);
+	});
 
-jQuery(document).bind('omeka:elementformload', function (event) {
-    Omeka.Elements.makeElementControls(event.target, <?php echo js_escape(url('elements/element-form')); ?>,'Item'<?php if ($id = metadata('item', 'id')) echo ', '.$id; ?>);
-    Omeka.Elements.enableWysiwyg(event.target);
-});
+	jQuery(document).bind('omeka:elementformload', function (event) {
+		Omeka.Elements.makeElementControls(event.target, <?php echo js_escape(url('elements/element-form')); ?>,'Item'<?php if ($id = metadata('item', 'id')) echo ', '.$id; ?>);
+		Omeka.Elements.enableWysiwyg(event.target);
+	});
 </script>
 
 <section class="seven columns alpha" id="edit-form">
-
     <?php echo flash(); ?>
     
     <div id="item-metadata">
@@ -46,6 +46,6 @@ jQuery(document).bind('omeka:elementformload', function (event) {
         <?php endif; ?>
     <?php endforeach; ?>
     </div>
-
 </section>
+
 <?php echo $csrf; ?>
