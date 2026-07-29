04852f8aa820c0f3cea9b19b6be3a922b09a6bc5
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2026-07-29 17:23:03 +0200

    Refactor item type form structure

--- views/admin/items/item-type-form.php
+++ views/admin/items/item-type-form.php
@@ -2,6 +2,7 @@
     <div class="two columns alpha">
         <?php echo $this->formLabel('item-type', __('Item Type')); ?>
     </div>
+
     <div class="inputs five columns omega">
     <?php
         echo $this->formSelect(
@@ -12,9 +13,12 @@
         );
     ?>
     </div>
-<input type="submit" name="change_type" id="change_type" value="<?php echo __('Pick this type'); ?>" />  
+
+	<input type="submit" name="change_type" id="change_type" value="<?php echo __('Pick this type'); ?>" />  
 </div>
+
 <div id="type-metadata-form">
-<?php echo common('change-type', compact('item'), 'items'); ?>
+	<?php echo common('change-type', compact('item'), 'items'); ?>
 </div>
+
 <?php fire_plugin_hook('admin_items_form_item_types', array('item' => $item, 'view' => $this)); ?>
